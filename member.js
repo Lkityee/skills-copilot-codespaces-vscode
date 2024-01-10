26481a35a10c59909a144c6e16a5980fd924f603
Lkityee <147017450+Lkityee@users.noreply.github.com>
2024-01-10 08:18:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skills-member");
+    var skillsMemberClose = document.getElementById("skills-member-close");
+    skillsMember.style.display = "block";
+    skillsMemberClose.style.display = "block";
+    skills.style.display = "none";
+}
